57818aa3c7852056fd4a3b7f7af1656cb67d22f2
leornado <leornado@126.com>
2018-06-11 16:44:22 +0800

    ...

--- src/js/bootstrap-cascader.js
+++ src/js/bootstrap-cascader.js
@@ -3,6 +3,7 @@
 
   // DEF_OPTS
   var DEF_OPTS = {
+    forceSelect: false,
     splitChar: ' ',
     cls: '',
     btnCls: 'btn-default',
@@ -28,7 +29,7 @@
         <a href="javascript:">\
           <span class="text"></span>\
           <span class="bscascader-font icon-ico-right-arrow item-right-arrow"></span>\
-          <span class="bscascader-font icbscascader-fonton-loading item-loading"></span>\
+          <span class="bscascader-font icon-loading item-loading"></span>\
           <span class="bscascader-font icon-error"></span>\
         </a>\
       </li>'
@@ -39,7 +40,7 @@
   };
 
   // DropdownPanel
-  var DropdownPanel = function (data, csd) {
+  var DropdownPanel = function (data, csd, setValueCallback) {
     if (!data || !data.children || data.children.length <= 0) return;
 
     var panel = this;
@@ -54,8 +55,8 @@
       if (item.loaded && item.children.length == 0) itemEl.addClass('no-child');
     };
 
-    this.selectItemByCode = function (code) {
-      panel.panelEl.children('li[code=' + enHtml(code) + ']').trigger('selectItem');
+    this.selectItemByCode = function (code, setValueCallback) {
+      panel.panelEl.children('li[code=' + enHtml(code) + ']').trigger('selectItem', [setValueCallback]);
     };
 
     this.setSelected = function (item, setOpen) {
@@ -78,7 +79,8 @@
     };
 
     this.scrollToOpened = function () {
-      var panelEl = panel.panelEl, selectedItem = panelEl.find('li a.selected').parent();
+      var panelEl = panel.panelEl, selectedItem = panelEl.find('li.open');
+      if (selectedItem.size() <= 0) selectedItem = panelEl.find('li a.selected').parent();
       if (selectedItem.size() > 0) {
         var scrollTop = panelEl.scrollTop(), top = selectedItem.position().top + scrollTop;
         if (scrollTop < top) {
@@ -100,7 +102,7 @@
       panel.panelEl.css({left: lastPanelLeft + lastPanelEl.outerWidth()})
     };
 
-    var handler = function (item, itemEl, selectItem) {
+    var handler = function (item, itemEl, selectItem, setValueCallback) {
       if (csd.params.lazy && item.loaded === false) {
         itemEl.addClass('bs-loading');
         var itemCode = item.code || item.c;
@@ -110,7 +112,7 @@
 
         csd.loadData(item).then(function () {
           if (panel.loadingItem == itemCode)
-            csd.refreshPanels(item.level + 1, item);
+            csd.refreshPanels(item.level + 1, item, setValueCallback);
         }, function () {
           if (panel.loadingItem == itemCode)
             itemEl.addClass('load-error');
@@ -122,7 +124,7 @@
           itemEl.removeClass('bs-loading');
         });
       } else {
-        csd.refreshPanels(item.level + 1, item);
+        csd.refreshPanels(item.level + 1, item, setValueCallback);
         panel.setItemOpened(item, itemEl);
         if (selectItem) panel.selectItem(item, itemEl);
       }
@@ -154,8 +156,8 @@
       itemEl.data("cascaderItem", item).attr('code', itemCode).attr('title', itemName).find('.text').text(itemName);
 
       itemEl.on({
-        'selectItem click': function () {
-          handler(item, itemEl, true);
+        'selectItem click': function (e, setValueCallback) {
+          handler(item, itemEl, true, setValueCallback);
         },
         'openDropdown': function () {
           handler(item, itemEl);
@@ -178,24 +180,28 @@
         if (panel.isMatchedSelectedItems(item, false)) {
           hasMatchedChildren = true;
           if (csd.selectedItems.length > item.level) {
-            handler(item, itemEl, false);
+            handler(item, itemEl, false, setValueCallback);
           } else {
             if (csd.selectedItems.length == csd.panels.length)
               $.each(csd.selectedItems, function (j, selectedItem) {
                 csd.panels[j].setSelected(selectedItem, j < csd.selectedItems.length - 1);
               });
-            csd.inited(), csd.reloaded();
+            csd.fireOnInit(), csd.reloaded();
+            if (setValueCallback) setValueCallback(true);
           }
         }
       }
     });
 
-    if ((!csd.isInited || csd.reloading) && csd.selectedItems.length > data.level && !hasMatchedChildren && csd.params.lazy)
-      csd.isInited ? csd.reloaded() : csd.inited();
+    if (csd.selectedItems.length > data.level && !hasMatchedChildren && csd.params.lazy) {
+      if ((!csd.isInited || csd.reloading))
+        csd.isInited ? csd.reloaded() : csd.fireOnInit();
+      if (setValueCallback) setValueCallback(false);
+    }
   };
 
   // Cascader
-  var Cascader = function (params) {
+  var Cascader = function (params, originEl) {
     var csd = this;
     params = params || {};
     for (var def in DEF_OPTS) if (typeof params[def] === 'undefined') params[def] = DEF_OPTS[def];
@@ -220,13 +226,15 @@
     var convertData2Col = function (data, parent) {
       var isLazy = csd.params.lazy;
       $.each(data, function (i, item) {
-        var itemCode = item.code || item.c, children = item.data || item.d;
-        var itemData = {
-          childMap: {}, children: [], loaded: !isLazy || item.hasChild === false,
-          level: parent.level + 1, parent: parent, originData: item
-        };
-        parent.children.push(itemData);
-        parent.childMap[itemCode] = itemData;
+        var itemCode = item.code || item.c, children = item.data || item.d, itemData = parent.childMap[itemCode];
+        if (!itemData) {
+          itemData = {
+            childMap: {}, children: [], loaded: !isLazy || (children && children.length > 0) || item.hasChild === false,
+            level: parent.level + 1, parent: parent, originData: item
+          };
+          parent.children.push(itemData);
+          parent.childMap[itemCode] = itemData;
+        } else itemData.loaded = !isLazy || (children && children.length > 0) || item.hasChild === false;
         if (children && children.length > 0) convertData2Col(children, itemData);
       });
     };
@@ -247,28 +255,57 @@
     };
 
     // setValue
-    csd.setValue = function (value) {
+    csd.setValue = function (value, opts) {
+      opts = $.extend({onSetFinish: $.noop}, opts || {});
+      var names = [], oldSelectedItems = csd.getValue();
       csd.clearValue();
+      if (!value || !(value instanceof Array) || value.length <= 0) {
+        opts.fireInit ? csd.fireOnInit() : csd.fireOnChange(oldSelectedItems, csd.getValue());
+        return;
+      }
 
-      var names = [], oldSelectedItems = csd.getValue;
-      $.each(value, function (i, item) {
-        csd.selectedItems.push({code: item.code || item.c, name: item.name || item.n});
-        names.push(item.name || item.n);
-      });
-      updateBtnText(names.join(csd.params.splitChar));
-      csd.updateViewBySelected();
-
-      csd.tryFireOnChange(oldSelectedItems, csd.getValue());
+      if (csd.params.forceSelect) {
+        var updateViewByLayzData = function () {
+          $.each(value, function (i, item) {
+            csd.selectedItems.push({code: item.code || item.c, name: item.name || item.n});
+            names.push(item.name || item.n);
+          });
+          csd.updateViewBySelected(function (allMatched) {
+            if (allMatched) {
+              updateBtnText(names.join(csd.params.splitChar));
+            } else csd.clearValue(false);
+            opts.fireInit ? csd.fireOnInit() : csd.fireOnChange(oldSelectedItems, csd.getValue());
+            opts.onSetFinish.call(csd);
+          });
+        };
+        if (csd.params.lazy && csd.params.loadDataByTreePath) {
+          csd.loadDataByTreePath(value).then(function () {
+            updateViewByLayzData();
+          }, function () {
+            opts.fireInit ? csd.fireOnInit() : csd.fireOnChange(oldSelectedItems, csd.getValue());
+            opts.onSetFinish.call(csd);
+          });
+        } else updateViewByLayzData();
+      } else {
+        $.each(value, function (i, item) {
+          csd.selectedItems.push({code: item.code || item.c, name: item.name || item.n});
+          names.push(item.name || item.n);
+        });
+        updateBtnText(names.join(csd.params.splitChar));
+        csd.updateViewBySelected();
+        opts.fireInit ? csd.fireOnInit() : csd.fireOnChange(oldSelectedItems, csd.getValue());
+        opts.onSetFinish.call(csd);
+      }
     };
 
     // refreshPanels
-    csd.refreshPanels = function (panelNo, data) {
+    csd.refreshPanels = function (panelNo, data, setValueCallback) {
       var rmPanels = csd.panels.splice(panelNo - 1, csd.panels.length);
       if (rmPanels) $.each(rmPanels, function (i, rmPanel) {
         rmPanel.destroy();
       });
 
-      new DropdownPanel(data, csd);
+      new DropdownPanel(data, csd, setValueCallback);
     };
 
     // destroy
@@ -299,23 +336,24 @@
       csd.close();
 
       var newItems = csd.getValue();
-      csd.tryFireOnChange(oldSelectedItems, newItems);
+      csd.fireOnChange(oldSelectedItems, newItems);
       csd.params.el.trigger('bs.cascader.select', [newItems]);
     };
 
-    // tryFireOnChange
-    csd.tryFireOnChange = function (oldItems, newItems) {
+    // fireOnChange
+    csd.fireOnChange = function (oldItems, newItems) {
       var fire = true;
       if (oldItems != newItems && oldItems.length == newItems.length) {
-        var allCodesSame = true;
+        var allItemsSame = true;
         $.each(oldItems, function (i, oldItem) {
-          var oldItemCode = oldItem.code || oldItem.c, newItem = newItems[i], newItemCode = newItem.code || newItem.c;
-          if (oldItemCode != newItemCode) {
-            allCodesSame = false;
+          var oldItemCode = oldItem.code || oldItem.c, oldItemName = oldItem.name || oldItem.n;
+          var newItem = newItems[i], newItemCode = newItem.code || newItem.c, newItemName = newItem.name || newItem.n;
+          if (oldItemCode != newItemCode || oldItemName != newItemName) {
+            allItemsSame = false;
             return false;
           }
         });
-        if (allCodesSame) fire = false;
+        if (allItemsSame) fire = false;
       }
       if (fire) csd.params.el.trigger('bs.cascader.change', [oldItems, newItems]);
     };
@@ -327,9 +365,9 @@
     };
 
     // update view by selected
-    csd.updateViewBySelected = function () {
+    csd.updateViewBySelected = function (setValueCallback) {
       if (csd.selectedItems.length > 0 && csd.panels.length > 0)
-        csd.panels[0].selectItemByCode(csd.selectedItems[0].code || csd.selectedItems[0].c);
+        csd.panels[0].selectItemByCode(csd.selectedItems[0].code || csd.selectedItems[0].c, setValueCallback);
     };
 
     // open
@@ -395,12 +433,28 @@
       csd.params.el.trigger('bs.cascader.reloaded');
     };
 
-    // inited
-    csd.inited = function () {
+    // fireOnInit
+    csd.fireOnInit = function () {
       if (csd.isInited) return;
 
       csd.isInited = true;
       csd.params.el.trigger('bs.cascader.inited');
+    };
+
+    // loadDataByTreePath
+    csd.loadDataByTreePath = function (items) {
+      var defered = $.Deferred();
+      csd.params.loadDataByTreePath.call(csd, items, function (data) {
+        if (data) {
+          if (data.length > 0) {
+            if (!csd.data.originData) csd.data.originData = data;
+            convertData2Col(data, csd.data);
+            csd.data.loaded = true;
+            defered.resolve(data);
+          } else defered.reject();
+        } else defered.reject();
+      });
+      return defered.promise();
     };
 
     // loadData
@@ -439,10 +493,11 @@
     initBtn();
     if (csd.params.readonly) csd.setReadonly(true);
 
+    originEl.data("bsCascader", this);
     csd.loadData().always(function () {
       csd.refreshPanels(1, csd.data);
-      if (csd.params.value) csd.setValue(csd.params.value);
-      if (!csd.params.lazy || !csd.params.value) csd.inited();
+      if (csd.params.value) csd.setValue(csd.params.value, {fireInit: true});
+      else csd.fireOnInit();
     });
 
     $('html').on('click', htmlClickHandler);
@@ -459,8 +514,7 @@
         params = $.extend({
           el: $this, value: $this.val() ? $this.val().split(params.splitChar || ' ') : ''
         }, params);
-        bsCascader = new Cascader(params);
-        $this.data("bsCascader", bsCascader);
+        bsCascader = new Cascader(params, $this);
       }
       if (bsCascader && typeof params === typeof "")
         bsCascader[params].apply(bsCascader, Array.prototype.slice.call(args, 1));
--- src/js/index.js
+++ src/js/index.js
@@ -19,85 +19,204 @@ $(function () {
         {c: 'chou', n: '丑', d: [{c: 'v', n: 'V'}, {c: 'w', n: 'W'}, {c: 'x', n: 'X'}]}]
     }];
 
-  $('#cascader1').bsCascader({
+  var onchange = function (valueId) {
+    return function (e, oldValue, newValue) {
+      console.log('=====onchange======', valueId, newValue);
+      $('#' + valueId).text(JSON.stringify(newValue));
+    };
+  };
+
+  var oninit = function (csdId, valueId) {
+    return function () {
+      console.log('=====oninit======', csdId);
+      var value = $('#' + csdId).data('bsCascader').getValue();
+      $('#' + valueId).text(JSON.stringify(value));
+    };
+  };
+
+  var onselect = onclear = onreload = function (eventType, bsId) {
+    return function () {
+      console.log('====' + eventType + '====', bsId)
+    }
+  };
+
+  var getListeners = function (csdId, valueId) {
+    return {
+      'bs.cascader.change': onchange(valueId), 'bs.cascader.inited': oninit(csdId, valueId),
+      'bs.cascader.select': onselect('onselect', csdId), 'bs.cascader.reloaded': onreload('onreload', csdId),
+      'bs.cascader.clear': onclear('onclear', csdId)
+    }
+  };
+
+  // cascader1
+  $('#cascader1').on($.extend(getListeners('cascader1', 'value1'), {})).bsCascader({
     openOnHover: true,
     loadData: function (openedItems, callback) {
       callback(localData);
     }
   });
+  $('#cascader1Btn').click(function () {
+    $('#cascader1').bsCascader('setValue', [{"code": "a", "name": "1"}, {"code": "b", "name": "2"},
+      {"code": "c", "name": "3"}]);
+  });
 
-  $('#cascader2').bsCascader({
+  // cascader1_2
+  $('#cascader1_2').on($.extend(getListeners('cascader1_2', 'value1_2'), {})).bsCascader({
+    openOnHover: true, forceSelect: true,
+    loadData: function (openedItems, callback) {
+      callback(localData);
+    }
+  });
+  $('#cascader1_2Btn').click(function () {
+    $('#cascader1_2').bsCascader('setValue', [{"code": "a", "name": "1"}, {"code": "b", "name": "2"},
+      {"code": "c", "name": "3"}]);
+  });
+  $('#cascader1_2Btn2').click(function () {
+    $('#cascader1_2').bsCascader('setValue', [{c: 'qian', n: '钱'}, {c: 'ji', n: '己'}, {c: 'h', n: 'H'}]);
+  });
+
+  // cascader2
+  $('#cascader2').on($.extend(getListeners('cascader2', 'value2'), {})).bsCascader({
     value: [{c: 'qian', n: '钱'}, {c: 'ji', n: '己'}, {c: 'h', n: 'H'}],
     loadData: function (openedItems, callback) {
       callback(localData);
     }
-  }).on('bs.cascader.change', function (e, oldValue, newValue) {
-    $('#cascaderValue2').text(JSON.stringify(newValue));
   });
 
   var NUMS = '零一二三四五六七八九'.split('');
+  var createMockData = function (openedItems) {
+    var level = openedItems.length + 1, data = [];
+    if (level > 4) return data;
+
+    var parentCode = '', parentName = '';
+    if (openedItems.length > 0) {
+      var parentItem = openedItems[openedItems.length - 1];
+      parentCode = parentItem.c || parentItem.code;
+      parentName = parentItem.n || parentItem.name;
+    }
+
+    $.each(NUMS, function (i, num) {
+      var item = {c: parentCode + i, n: parentName + num};
+      if (level == 4) item.hasChild = false;
+      data.push(item);
+    });
+    return data;
+  };
   var mockLazyLoadFn = function () {
     return function (openedItems, callback) {
+      console.log('------mockLazyLoadFn------');
       setTimeout(function () {
-        var level = openedItems.length + 1, data = [];
-        if (level > 4) return callback(data);
-
-        var parentCode = '', parentName = '';
-        if (openedItems.length > 0) {
-          parentCode = openedItems[openedItems.length - 1].c;
-          parentName = openedItems[openedItems.length - 1].n;
-        }
-
-        $.each(NUMS, function (i, num) {
-          var item = {c: parentCode + i, n: parentName + num};
-          if (level == 4) item.hasChild = false;
-          data.push(item);
-        });
-        callback(data);
+        callback(createMockData(openedItems));
       }, 500);
     }
   };
 
-  $('#cascader3').bsCascader({
+  // cascader3
+  $('#cascader3').on($.extend(getListeners('cascader3', 'value3'), {})).bsCascader({
     splitChar: ' / ',
     openOnHover: true,
     lazy: true,
     loadData: mockLazyLoadFn()
   });
 
-  $('#cascader4').bsCascader({
-    splitChar: ' / ',
-    lazy: true,
-    dropUp: true,
-    value: [{"code": "8", "name": "八"}, {"code": "81", "name": "八一"},
-      {"code": "818", "name": "八一八"}, {"code": "8187", "name": "八一八七"}],
-    loadData: mockLazyLoadFn()
-  }).on({
-    'bs.cascader.change bs.cascader.select': function (e, oldValue, newValue) {
-      $('#cascaderValue4').text(JSON.stringify(newValue));
-    },
-    'bs.cascader.inited bs.cascader.reloaded': function () {
-      $('#cascader4Btn').removeClass('disabled');
-    }
-  });
+  // cascader4
+  $('#cascader4')
+      .on($.extend(getListeners('cascader4', 'value4'), {}))
+      .on('bs.cascader.inited bs.cascader.reloaded', function () {
+        $('#cascader4Btn').removeClass('disabled');
+      })
+      .bsCascader({
+        splitChar: ' / ',
+        lazy: true,
+        dropUp: true,
+        value: [{"code": "8", "name": "八"}, {"code": "81", "name": "八一"},
+          {"code": "818", "name": "八一八"}, {"code": "8187", "name": "八一八七"}],
+        loadData: mockLazyLoadFn()
+      });
 
   $('#cascader4Btn').addClass('disabled').click(function () {
     $(this).addClass('disabled');
     $('#cascader4').bsCascader('reload');
   });
 
-
-  $('#cascader5').bsCascader({
-    splitChar: ' / ',
-    lazy: true,
-    dropUp: true,
-    loadData: mockLazyLoadFn()
-  }).on('bs.cascader.inited bs.cascader.reloaded', function () {
-    $('#cascader5Btn').removeClass('disabled');
-  });
-
+  // cascader5
+  $('#cascader5')
+      .on($.extend(getListeners('cascader5', 'value5'), {}))
+      .bsCascader({
+        splitChar: ' / ',
+        lazy: true,
+        dropUp: true,
+        loadData: mockLazyLoadFn()
+      });
   $('#cascader5Btn').click(function () {
     $('#cascader5').bsCascader('setValue', [{"code": "8", "name": "八"}, {"code": "81", "name": "八一"},
+      {"code": "81b", "name": "八一B"}, {"code": "81ba", "name": "八一BA"}]);
+  });
+  $('#cascader5Btn2').click(function () {
+    $('#cascader5').bsCascader('setValue', [{"code": "8", "name": "八"}, {"code": "81", "name": "八一"},
+      {"code": "818", "name": "八一八"}, {"code": "8187", "name": "八一八七"}]);
+  });
+
+  // cascader6
+  $('#cascader6')
+      .on($.extend(getListeners('cascader6', 'value6'), {}))
+      .bsCascader({
+        forceSelect: true,
+        splitChar: ' / ',
+        lazy: true,
+        dropUp: true,
+        loadData: mockLazyLoadFn()
+      });
+  $('#cascader6Btn').click(function () {
+    $('#cascader6').bsCascader('setValue', [{"code": "8", "name": "八"}, {"code": "81", "name": "八一"},
+      {"code": "81b", "name": "八一B"}, {"code": "81ba", "name": "八一BA"}]);
+  });
+  $('#cascader6Btn2').click(function () {
+    $('#cascader6').bsCascader('setValue', [{"code": "8", "name": "八"}, {"code": "81", "name": "八一"},
+      {"code": "818", "name": "八一八"}, {"code": "8187", "name": "八一八七"}]);
+  });
+
+  // cascader7
+  $('#cascader7')
+      .on($.extend(getListeners('cascader7', 'value7'), {}))
+      .bsCascader({
+        forceSelect: true,
+        splitChar: ' / ',
+        lazy: true,
+        dropUp: true,
+        loadData: mockLazyLoadFn(),
+        loadDataByTreePath: function (items, callback) {
+          setTimeout(function () {
+            var data = [], checkedItems = [], lastMatchedItem, allMatched = true;
+            for (var i = 0; i < items.length; i++) {
+              var item = items[i], itemCode = item.code || item.c, matchedItem = false;
+              var mockData = createMockData(checkedItems);
+              $.each(mockData, function (j, mockItem) {
+                var mockItemCode = mockItem.code || mockItem.c;
+                if (mockItemCode == itemCode) {
+                  matchedItem = mockItem;
+                  return false;
+                }
+              });
+              if (!matchedItem) {
+                allMatched = false;
+                break;
+              }
+
+              lastMatchedItem ? lastMatchedItem.data = mockData : data = mockData;
+              lastMatchedItem = matchedItem;
+              checkedItems.push(item);
+            }
+            callback(allMatched ? data : []);
+          }, 500);
+        }
+      });
+  $('#cascader7Btn').click(function () {
+    $('#cascader7').bsCascader('setValue', [{"code": "8", "name": "八"}, {"code": "82", "name": "八二"},
+      {"code": "82b", "name": "八二B"}, {"code": "82ba", "name": "八二BA"}]);
+  });
+  $('#cascader7Btn2').click(function () {
+    $('#cascader7').bsCascader('setValue', [{"code": "8", "name": "八"}, {"code": "81", "name": "八一"},
       {"code": "818", "name": "八一八"}, {"code": "8187", "name": "八一八七"}]);
   });
 
